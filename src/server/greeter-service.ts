be21289d3c60e288eb73a8539a9e81467ea789b1
Daniel Mateiu <daniel.mateiu@redbull.com>
2023-01-29 13:19:05 +0100

    build: add traceing logs for debugging purposes
    Disable no console eslint rule for file as no other logger has been implemented yet

--- src/server/greeter-service.ts
+++ src/server/greeter-service.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 import type * as grpc from 'grpc'
 import type { sendUnaryData } from 'grpc'
 
@@ -9,10 +11,14 @@ export class GreetServerService implements IGreeterServer {
   // eslint-disable-next-line promise/prefer-await-to-callbacks
   sayHello(call: grpc.ServerUnaryCall<HelloRequest>, callback: sendUnaryData<HelloReply>): void {
 
+    console.trace('Invoked "sayHello" function on server')
+
     const reply = new HelloReply()
     const name = call.request.getName()
 
     reply.setMessage(`Hello ${name}`)
+
+    console.trace(`Reply to client with message: ${reply.getMessage()}`)
 
     // eslint-disable-next-line promise/prefer-await-to-callbacks
     callback(null, reply)
